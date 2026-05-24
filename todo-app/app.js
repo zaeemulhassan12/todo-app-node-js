b5cb238ae24580c42ab5b0f876616a4a00fdfc85
zaeemulhassan12 <zaeemulhassan819.com>
2026-05-24 19:13:19 +0500

    add a packages or build a crude operations

---
+++ todo-app/app.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const sqlite3 = require("sqlite3").verbose();
+const path = require("path");
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.set("view engine", "ejs");
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
+const db = new sqlite3.Database("./database/todo.db");
+
+db.run(`
+CREATE TABLE IF NOT EXISTS todos (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    task TEXT NOT NULL,
+    priority TEXT DEFAULT 'Medium',
+    completed INTEGER DEFAULT 0
+)
+`);
+
+app.get("/", (req, res) => {
+    db.all("SELECT * FROM todos", [], (err, rows) => {
+        if (err) {
+            throw err;
+        }
+
+        res.render("index", { todos: rows });
+    });
+});
+
+app.post("/add", (req, res) => {
+    const { task, priority } = req.body;
+
+    db.run(
+        "INSERT INTO todos (task, priority) VALUES (?, ?)",
+        [task, priority],
+        (err) => {
+            if (err) {
+                console.log(err);
+            }
+
+            res.redirect("/");
+        }
+    );
+});
+
+app.get("/delete/:id", (req, res) => {
+    db.run(
+        "DELETE FROM todos WHERE id = ?",
+        [req.params.id],
+        (err) => {
+            if (err) {
+                console.log(err);
+            }
+
+            res.redirect("/");
+        }
+    );
+});
+
+app.get("/edit/:id", (req, res) => {
+    db.get(
+        "SELECT * FROM todos WHERE id = ?",
+        [req.params.id],
+        (err, row) => {
+            res.render("edit", { todo: row });
+        }
+    );
+});
